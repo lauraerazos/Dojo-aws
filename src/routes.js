0cddfc7fa5b014bfaac5eac1da7e47647063962e
jhndagon <johanc.suarez@hotmail.com>
2020-02-29 04:47:43 -0500

    first commit

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const getTodos  = require('./services/get-todos');
+const router = express.Router();
+
+router.get('/todos', getTodos);
+
+module.exports = router;
